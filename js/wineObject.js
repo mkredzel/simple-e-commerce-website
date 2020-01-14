94ffa8fe75e27823fa82ff6f5e3cd8e614304b39
Marcel Kredzel <44983911+mkredzel@users.noreply.github.com>
2020-01-14 20:29:56 +0000

    All merged together with all imgs and background, amazing nav bar too!

---
+++ js/wineObject.js
@@ -0,0 +1,9 @@
+function Wine(name, type, price, country, abv, description, image) {
+    this.wineName = name;
+    this.wineType = type;
+    this.winePrice = price;
+    this.wineCountry = country;
+    this.wineAbv = abv;
+    this.wineDescription = description;
+    this.wineImage = image;
+}
